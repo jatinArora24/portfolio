c20f01822f84e38a716018c2bfa3641045b5630e
Jatin Arora <jatinarora2499@gmail.com>
2024-09-08 12:36:06 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -4,17 +4,29 @@ import SectionRender from "./components/sectionRender";
 function App() {
   return (
     <div className="overflow-y-auto snap-y snap-mandatory scroll-smooth h-screen w-full">
-      <SectionRender
-        className={
-          "h-75vh w-full bg-revolver-200 flex justify-center snap-start"
-        }
-      >
-        <a className="underline" href="/">
+      <div className="rounded-full h-20 w-20 p-3 bg-revolver-100 text-gold-300 flex justify-center items-center border-b-[86px] border-l-[60vw] border-revolver-400 fixed">
+        {"<Jatin Arora/>"}
+      </div>
+
+      <nav className="w-full h-16 bg-revolver-100 fixed border-2 border-revolver-400 rounded-bl rounded-br">
+        <a
+          style={{ textShadow: "1px 1px #2e1e00" }}
+          className="rounded-full h-20 w-20 p-3 bg-revolver-100 text-gold-300 flex justify-center items-center"
+          href="/"
+        >
           {"<Jatin Arora/>"}
         </a>
+      </nav>
+      <SectionRender
+        className={
+          "h-75vh w-full bg-revolver-700 flex justify-around snap-start pl-24 pr-24 pt-32 pb-32 flex-col"
+        }
+      >
         <div>
-          <h1 className="text-3xl font-bold">Frontend Developer.</h1>
-          <p>
+          <h1 className="text-7xl font-bold text-shadow-lg text-gold-50">
+            Frontend Developer.
+          </h1>
+          <p className="text-gold-100">
             I like to craft solid and scalable frontend products with great user
             experiences.
           </p>
@@ -22,7 +34,21 @@ function App() {
       </SectionRender>
       <SectionRender
         className={
-          "h-75vh w-full bg-revolver-900 flex justify-center snap-start"
+          "h-75vh w-full bg-revolver-100 flex justify-center snap-start pl-24 pr-24 pt-32 pb-32"
+        }
+      >
+        <div>hello world</div>
+      </SectionRender>
+      <SectionRender
+        className={
+          "h-75vh w-full bg-revolver-300 flex justify-center snap-start pl-24 pr-24 pt-32 pb-32"
+        }
+      >
+        <div>hello world</div>
+      </SectionRender>
+      <SectionRender
+        className={
+          "h-screen w-full bg-revolver-900 flex justify-center snap-start pl-24 pr-24 pt-32 pb-32"
         }
       >
         <div>hello world</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        "text-md": "1px 1px 2px rgba(0, 0, 0, 0.5)", // Medium shadow
+        "text-lg": "2px 2px 4px rgba(0, 0, 0, 0.5)", // Large shadow
+        "text-xl": "3px 3px 6px rgba(0, 0, 0, 0.5)", // Extra large shadow
+      },
       height: {
         "75vh": "75vh",
       },
@@ -19,6 +24,19 @@ module.exports = {
           800: "#883b98",
           900: "#6c3679",
           950: "#26132a",
+        },
+        gold: {
+          50: "#fdf6e3",
+          100: "#fceeb5",
+          200: "#f9d67e",
+          300: "#f7b848",
+          400: "#f5a020",
+          500: "#e69700",
+          600: "#b87e00",
+          700: "#8d6400",
+          800: "#6b4b00",
+          900: "#4a3200",
+          950: "#2e1e00",
         },
       },
     },
